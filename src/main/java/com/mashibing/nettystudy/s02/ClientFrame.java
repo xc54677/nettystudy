b4a96b1ac90f97dff7093f56df8811b135f7d5c6
xiao <gesangcx@outlook.com>
2020-08-26 22:24:37 +0800

    添加一个聊天室窗口

---
+++ src/main/java/com/mashibing/nettystudy/s02/ClientFrame.java
@@ -0,0 +1,40 @@
+package com.mashibing.nettystudy.s02;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class ClientFrame extends Frame {
+    TextArea ta = new TextArea();
+    TextField tf = new TextField();
+
+    public ClientFrame(){
+        this.setSize(600, 400);
+        this.setLocation(100, 20);
+        this.add(ta, BorderLayout.CENTER);
+        this.add(tf, BorderLayout.SOUTH);
+
+        tf.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ta.setText(ta.getText() + "\n" + tf.getText());
+                tf.setText("");
+            }
+        });
+
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+
+        this.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new ClientFrame();
+    }
+}
